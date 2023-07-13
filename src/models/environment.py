8c60cb0634e40e264c0d257021c5004e2a90b88c
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-07-13 21:02:37 +0100

    refactor

--- src/models/environment.py
+++ src/models/environment.py
@@ -4,5 +4,9 @@ from src.models.car import Car
 
 
 class Environment:
+    """
+    A class used to represent the environment,
+    with its cars and representative data.
+    """
     def __init__(self):
         self.cars: List[Car] = []
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -8,20 +8,20 @@ ROOT_DIR = os.path.dirname(
         os.path.dirname(__file__)
     )
 )
-LOG_FILE = os.path.join(ROOT_DIR, f'{USER}.log')
+LOG_FILE = os.path.join(ROOT_DIR, f"{USER}.log")
 
 
 class CustomFormatter(logging.Formatter):
-    FORMAT = '%(asctime)s [%(pathname)s:%(lineno)d] %(levelname)-8s | %(message)s'
+    FORMAT = "%(asctime)s [%(pathname)s:%(lineno)d] %(levelname)-8s | %(message)s"
     LEVEL_COLORS = {
-        'DEBUG': '\033[0;36m',  # Cyan
-        'INFO': '\033[0;37m',  # White
-        'WARNING': '\033[0;33m',  # Yellow
-        'ERROR': '\033[0;31m',  # Red
-        'CRITICAL': '\033[1m\033[1;31m'  # Bold Red
+        "DEBUG": "\033[0;36m",  # Cyan
+        "INFO": "\033[0;37m",  # White
+        "WARNING": "\033[0;33m",  # Yellow
+        "ERROR": "\033[0;31m",  # Red
+        "CRITICAL": "\033[1m\033[1;31m"  # Bold Red
     }
-    RESET = '\033[0m'
-    TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+    RESET = "\033[0m"
+    TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
     def __init__(self, colored: bool = None):
         super().__init__(self.FORMAT)
@@ -29,12 +29,12 @@ class CustomFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord):
         if self.colored:
-            log_format = self.LEVEL_COLORS.get(record.levelname, '') + self.FORMAT + self.RESET
+            log_format = self.LEVEL_COLORS.get(record.levelname, "") + self.FORMAT + self.RESET
         else:
             log_format = self.FORMAT
 
         # Truncate the pathname from the root directory
-        if 'pathname' in record.__dict__.keys() and ROOT_DIR in record.pathname:
+        if "pathname" in record.__dict__.keys() and ROOT_DIR in record.pathname:
             record.pathname = record.pathname[len(ROOT_DIR) + 1:]
 
         return logging.Formatter(log_format, self.TIME_FORMAT).format(record)
@@ -42,7 +42,7 @@ class CustomFormatter(logging.Formatter):
 
 class LogFilter(logging.Filter):
     def filter(self, record):
-        return all(map(lambda keyword: keyword not in record.name, ['botocore', 'urllib', 's3transfer', 'playsound']))
+        return all(map(lambda keyword: keyword not in record.name, ["botocore", "urllib", "s3transfer", "playsound"]))
 
 
 class CustomLogger(logging.Logger):
@@ -62,7 +62,7 @@ logging.setLoggerClass(CustomLogger)
 
 logger = logging.getLogger("EEIA 2023")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logger.debug("A debug")
     logger.info("An info")
     logger.warning("A warning")
